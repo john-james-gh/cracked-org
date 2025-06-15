244d6ecafb0b9ffa7f031ce18da762890fee2ed8
James <john.usa.james@gmail.com>
2025-06-15 13:01:55 +0100

    chore(config): add Prettier and lint-staged configurations, update package.json for formatting and pre-commit hooks

---
+++ lint-staged.config.mjs
@@ -0,0 +1,9 @@
+/**
+ * @see https://github.com/lint-staged/lint-staged?tab=readme-ov-file#configuration
+ * @type {import('lint-staged').Configuration}
+ */
+const config = {
+  "**/*": "prettier --write --ignore-unknown",
+};
+
+export default config;
